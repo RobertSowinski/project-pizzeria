444458da5c10728af4c2d3fe051d82e5df7987ec
Robert Sowiński <saironeandonly@gmail.com>
2025-02-04 14:12:21 +0100

    add datepicker hourpicker (hourpicker doesn't work)

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import { select, templates } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 class Booking {
   constructor(element) {
     const thisBooking = this;
@@ -17,7 +18,7 @@ class Booking {
     
     // Tworzymy pusty obiekt thisBooking.dom
     thisBooking.dom = {};
-
+    
     // Przypisujemy kontener (wrapper) z argumentu
     thisBooking.dom.wrapper = element;
 
@@ -27,15 +28,26 @@ class Booking {
     // ETAP 2: Znajdź elementy w nowo utworzonym HTML
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+    // Dodanie referencji do DatePicker i HourPicker
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
   }
 
   initWidgets() {
     const thisBooking = this;
 
+    
     // Tworzymy instancje AmountWidget dla obu inputów
     thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
-    // Na razie nie dodajemy listenerów - zgodnie z instrukcją
+    
+    // Tworzymy instancje nowych widgetów
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+
+    //console.log('hourPicker: ', thisBooking.dom.hourPicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 
